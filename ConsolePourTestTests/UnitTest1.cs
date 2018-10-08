bffd6d4b2101672573834cc176607159235cee2d
AnnePRT <anne.parrot82@gmail.com>
2018-10-08 10:14:34 +0200

    creation des tests de base pour exemple

--- ConsolePourTestTests/UnitTest1.cs
+++ ConsolePourTestTests/UnitTest1.cs
@@ -9,6 +9,18 @@ namespace ConsolePourTestTests
 		[TestMethod]
 		public void TestMethod1()
 		{
+			int i = 1;
+			int j = 2;
+			Assert.AreEqual(3, i + j);// test pour voir si i+j=3
+		}
+
+		[TestMethod]
+		public void TestMethodAge()
+		{
+			var dateNaissance = new DateTime(2010, 5, 1);
+			int j = 2;
+			Assert.IsTrue(
+				DateTime.Today.Subtract(dateNaissance).Days/365<10);//test pour savoir si age<10
 		}
 	}
 }
